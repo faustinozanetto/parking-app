d415d63fc22d3449fd5a940d2a7c0587ae419dfa
Faustino Zanetto <faustizanetto@gmail.com>
2022-11-18 20:29:53 -0300

    Started working on client app with expo.

---
+++ client/App.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white text-red-400">
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text> Hello World</Text>
+    </View>
+  );
+}
